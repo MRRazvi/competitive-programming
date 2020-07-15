83602e14f7e03f903c59ffafd35947b3f540307d
rizimore <rizimore@gmail.com>
2020-07-15 15:09:16 +0500

    atcoder problem solution

--- atcoder/abc/a/ABC169.cpp
+++ atcoder/abc/a/ABC169.cpp
@@ -1,4 +1,11 @@
-//
-// Created by Mubashir R. Razv on 7/15/2020.
-//
+#include <iostream>
+using namespace std;
 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a, b;
+    cin >> a >> b;
+    cout << a*b << endl;
+    return 0;
+}
