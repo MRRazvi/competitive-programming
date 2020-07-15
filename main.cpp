90c188b03e2c045b1aea9965227c33dcd30716d8
rizimore <rizimore@gmail.com>
2020-07-15 15:26:08 +0500

    atcoder problem solution

--- main.cpp
+++ main.cpp
@@ -4,24 +4,16 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n;
-    cin >> n;
-    switch (n%10) {
-        case 2:
-        case 4:
-        case 5:
-        case 7:
-        case 9:
-            cout << "hon" << endl;
-            return 0;
-        case 0:
-        case 1:
-        case 6:
-        case 8:
-            cout << "pon" << endl;
-            return 0;
-        case 3:
-            cout << "bon" << endl;
+    string s, t;
+    cin >> s >> t;
+    if (s.length() + 1 != t.length()) {
+        cout << "No" << endl;
+        return 0;
     }
+    t.pop_back();
+    if (s == t)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
     return 0;
 }
