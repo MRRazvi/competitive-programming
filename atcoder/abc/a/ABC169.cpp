64560601985e2e21d59e159d6468f3d8775ca17b
rizimore <rizimore@gmail.com>
2020-07-15 15:08:56 +0500

    atcoder problem solution

---
+++ atcoder/abc/a/ABC169.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Mubashir R. Razv on 7/15/2020.
+//
+
--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#include <set>
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    set<int> s = {1, 2, 3, 4, 5, 0};
-    for (int i=0; i<5; i++) {
-        int num;
-        cin >> num;
-        s.erase(num);
-    }
-    cout << *s.begin() << endl;
+    int a, b;
+    cin >> a >> b;
+    cout << a*b << endl;
     return 0;
 }
