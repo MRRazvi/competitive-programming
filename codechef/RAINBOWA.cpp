3387d5b759c93b1be1a462d40cac12f3df336a2d
rizimore <rizimore@gmail.com>
2020-07-27 02:49:50 +0500

    Chef and Rainbow Array

--- codechef/RAINBOWA.cpp
+++ codechef/RAINBOWA.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -13,15 +14,21 @@ int main() {
         for (auto &input: a)
             cin >> input;
 
+        map<int, int> mp;
+        for (int i=0; i<7; i++)
+            mp.insert({i+1, 0});
+
         int flag = 1;
-        for (int i=0; i<n/2; i++) {
-            if (a[i] != a[a.size()-1-i]) {
+        for (int i=0; i<=n/2; i++) {
+            mp[a[i]]++;
+            if (a[i] != a[n-i-1])
                 flag = 0;
-            }
         }
 
-        if (a[(n/2)+1]+1 == a[n/2])
-            flag = 0;
+        for (auto m: mp) {
+            if (m.second == 0)
+                flag = 0;
+        }
 
         if (flag)
             cout << "yes";
