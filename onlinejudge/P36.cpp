2d76d7c45200e360ed2fb89380507aed3a8df6e7
rizimore <rizimore@gmail.com>
2020-07-31 15:28:13 +0500

    100 - The 3n + 1 problem

--- onlinejudge/P36.cpp
+++ onlinejudge/P36.cpp
@@ -4,10 +4,10 @@
 #include <vector>
 #include <algorithm>
 
-typedef long long int ll;
+typedef long long ll;
 using namespace std;
 
-int countCyclicLength(ll num) {
+ll countCyclicLength(ll num) {
     ll count = 1;
     while (num != 1) {
         if (num%2 == 0) num /= 2;
@@ -22,16 +22,22 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
 
     ll i, j;
     while (cin >> i >> j) {
         vector<ll> v;
-        for (ll k=i; k<=j; k++) {
-            v.push_back(countCyclicLength(k));
+        if (i < j) {
+            for (ll k=i; k<=j; k++) {
+                v.push_back(countCyclicLength(k));
+            }
+        } else {
+            for (ll k=i; k>=j; k--) {
+                v.push_back(countCyclicLength(k));
+            }
         }
         sort(v.rbegin(), v.rend());
         cout << i << " " << j << " " << v[0] << endl;
