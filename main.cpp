2d12dcc72af65ca7c638aa4faee9911cba57375e
rizimore <rizimore@gmail.com>
2020-08-18 13:14:21 +0500

    A. Expression

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+// A. Expression
+// https://codeforces.com/problemset/problem/479/A
+// 3 mint avg
+// basi math, array, sort, implementation
+
 #include <iostream>
 #include <vector>
 #include <set>
@@ -10,17 +15,16 @@ using namespace std;
 int main() {
     freopen("output.txt", "w", stdout);
 
-    int n, m;
-    cin >> n >> m;
-    vector<int> f(m);
-    for (auto &i: f)
-        cin >> i;
-    sort(f.begin(), f.end());
-
-    int best = f[n-1] - f[0];
-    for (int i=1; i<=m-n; i++) {
-        best = min(best, f[i+n-1]-f[i]);
-    }
-    cout << best << endl;
+    int a, b, c;
+    cin >> a >> b >> c;
+    vector<int> v;
+    v.push_back(a+b+c);
+    v.push_back(a+b*c);
+    v.push_back(a*b+c);
+    v.push_back(a*(b+c));
+    v.push_back((a+b)*c);
+    v.push_back(a*b*c);
+    sort(v.begin(), v.end());
+    cout << v[v.size()-1] << endl;
     return 0;
 }
