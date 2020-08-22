a1967460cfbbc6911014e933c4a87d5a402a0b8b
rizimore <rizimore@gmail.com>
2020-08-22 12:35:20 +0500

    update git ignore

--- main.cpp
+++ main.cpp
@@ -8,19 +8,6 @@ using namespace std;
 int main() {
     freopen("output.txt", "w", stdout);
 
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<int> profit;
-        for (int i=0; i<n; i++) {
-            int s, p, v;
-            cin >> s >> p >> v;
-            s++;
-            profit.push_back((p/s) * v);
-        }
-        cout << *max_element(profit.begin(), profit.end()) << endl;
-    }
+
     return 0;
 }
