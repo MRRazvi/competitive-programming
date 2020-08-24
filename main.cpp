a5706b3b0db9b8f3e6b7fb166cbc3b2a89c55d89
rizimore <rizimore@gmail.com>
2020-08-24 16:11:59 +0500

    organize files

--- main.cpp
+++ main.cpp
@@ -8,28 +8,6 @@ using namespace std;
 int main() {
     freopen("output.txt", "w", stdout);
 
-    int n;
-    cin >> n;
-    int matrix[n][n];
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
-            matrix[i][j] = 1;
-        }
-    }
 
-    for (int i=1; i<n; i++) {
-        for (int j=1; j<n; j++) {
-            matrix[i][j] = matrix[i-1][j] + matrix[i][j-1];
-        }
-    }
-
-    vector<int> ans;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
-            ans.push_back(matrix[i][j]);
-        }
-    }
-
-    cout << *max_element(ans.begin(), ans.end()) << endl;
     return 0;
 }
